ab4f6f42dddc8564be1740f1a3c5a99b5b0fa79c
Anders Birkenes <datagutten@datagutten.net>
2026-03-30 22:11:11 +0200

    Add __init__.py

---
+++ xmltv/__init__.py
@@ -0,0 +1,6 @@
+# noinspection PyUnusedImports
+from .channel import Channel
+# noinspection PyUnusedImports
+from .elements import SubTitle, Description, DisplayName, LocalizedElement
+# noinspection PyUnusedImports
+from .program import Program
